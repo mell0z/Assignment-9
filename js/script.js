4d9fcf0557f822d13d845e66a019ebe3aa8ba700
Your GitHub Username <zunigam@mtmary.edu>
2026-04-19 18:04:19 -0500

    extra credit added gif search functionality

--- js/script.js
+++ js/script.js
@@ -1,37 +1,35 @@
 console.log("script.js loaded");
 
 // Request URL
-let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=mVcNsWoJaxmBAhZ1ueaN2RYvLPhQZQjs&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+let endpointBase = "https://api.giphy.com/v1/gifs/search?api_key=mVcNsWoJaxmBAhZ1ueaN2RYvLPhQZQjs&limit=25";
 
 // Where the GIFs will be displayed
 let gifContainer = document.querySelector("#gif-container");
 // Fetch GIF button
 let button = document.querySelector("#fetch-gif-btn");
+// Search input field
+let searchInput = document.querySelector("#search-input");
 
-async function getGifs() {
-    // Sends request to Giphy API
-    const response = await fetch(endpoint);
-    // converts response to JSON
+button.addEventListener("click", async function () {
+    // Gets user input
+    let searchTerm = searchInput.value;
+    // Updates API endpoint with user input
+    let updatedEndpoint = endpointBase + `&q=${searchTerm}`;
+    // Fetches data using endpoint
+    const response = await fetch(updatedEndpoint);
     const data = await response.json();
-
-    let gifs = data.data; // Array of GIF objects
+    
+    let gifs = data.data;
     let images = [];
 
     for (let i = 0; i < gifs.length; i++) {
         images.push(gifs[i].images.original.url);
     }
 
-    return images;
-}
-
-button.addEventListener("click", async function () {
-    // Gets images from API
-    let images = await getGifs();
-    // Clears old gifs
+    // Displays the GIFs
     gifContainer.innerHTML = "";
-    // Loops through the images and displays them
+
     for (let i = 0; i < images.length; i++) {
         gifContainer.innerHTML += `<img src="${images[i]}" class="col-3 mb-3">`;
     }
 });
-
