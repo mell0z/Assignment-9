ac030015ed7f6036da9666ee9055d1017d22bd7f
Your GitHub Username <zunigam@mtmary.edu>
2026-04-19 17:28:10 -0500

    Connect to Giphy API and log response data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("script.js loaded");
+
+// Request URL
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=mVcNsWoJaxmBAhZ1ueaN2RYvLPhQZQjs&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+async function getGifs() {
+    // Sends request to Giphy API
+    const response = await fetch(endpoint);
+    // converts response to JSON
+    const data = await response.json();
+    // outputs the data to the console
+    console.log(data);
+}
+
+// test
+getGifs(); 
